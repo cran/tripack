c949e7dfd5cfeef1a50c2a6ecccabf7465645122
Albrecht Gebhardt <albrecht.gebhardt@uni-klu.ac.at>
2009-06-15 00:00:00 +0000

    version 1.3-4

--- src/init.c
+++ src/init.c
@@ -22,24 +22,24 @@ static R_NativePrimitiveArgType addcst_t[11] = {
   INTSXP   /* IER */
 };
 
-//static R_NativePrimitiveArgType awvoronoi_t[16] = {
-//  INTSXP,  /* NCC */
-//  INTSXP,  /* LCC */
-//  INTSXP,  /* N */
-//  REALSXP, /* X */
-//  REALSXP, /* Y */
-//  REALSXP, /* WT */
-//  INTSXP,  /* LIST */
-//  INTSXP,  /* LPTR */
-//  INTSXP,  /* LEND */
-//  INTSXP,  /* NT */
-//  REALSXP, /* LCCC */
-//  INTSXP,  /* ICCC */
-//  INTSXP,  /* LCT */
-//  INTSXP,  /* LTRI */
-//  INTSXP,  /* MAXIT */
-//  INTSXP   /* IER */
-//};
+static R_NativePrimitiveArgType awvoronoi_t[16] = {
+  INTSXP,  /* NCC */
+  INTSXP,  /* LCC */
+  INTSXP,  /* N */
+  REALSXP, /* X */
+  REALSXP, /* Y */
+  REALSXP, /* WT */
+  INTSXP,  /* LIST */
+  INTSXP,  /* LPTR */
+  INTSXP,  /* LEND */
+  INTSXP,  /* NT */
+  REALSXP, /* LCCC */
+  INTSXP,  /* ICCC */
+  INTSXP,  /* LCT */
+  INTSXP,  /* LTRI */
+  INTSXP,  /* MAXIT */
+  INTSXP   /* IER */
+};
 
 static R_NativePrimitiveArgType troutq_t[17] = {
   INTSXP,  /* NCC */
@@ -162,7 +162,7 @@ static R_NativePrimitiveArgType voronoi_t[14] = {
 static R_FortranMethodDef fortranMethods[] = {
   {"addcst", (DL_FUNC) &F77_SUB(addcst), 11, addcst_t}, 
     /* add.constraint */
-//  {"awvoronoi", (DL_FUNC) &F77_SUB(awvoronoi), 16, awvoronoi_t}, 
+  {"awvoronoi", (DL_FUNC) &F77_SUB(awvoronoi), 16, awvoronoi_t}, 
     /* aw.voronoi.mosaic */
   {"troutq", (DL_FUNC) &F77_SUB(troutq), 17, troutq_t}, 
     /* cells, neighbours, plot.tri, print.tri, summary.tri */
